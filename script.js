1a8b040e6009b22cf57e3df0ada9c159861328e9
aishtiaq <awshaf_ishtiaque@sfu.ca>
2024-07-06 14:21:25 -0700

    update

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
     const email = document.getElementById("email");
+    const password = document.getElementById("password");
+  
+    //email focus 
     email.addEventListener("focus", (event) => {
       event.target.closest('.input-wrapper').classList.add('focus');
     });
     email.addEventListener("blur", (event) => {
       event.target.closest('.input-wrapper').classList.remove('focus');
+    });
+  
+    //password focus 
+    password.addEventListener("focus", (event) => {
+      event.target.closest('.password-wrapper').classList.add('focus');
+    });
+    password.addEventListener("blur", (event) => {
+      event.target.closest('.password-wrapper').classList.remove('focus');
     });
   });
   
