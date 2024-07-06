597e51654ad54aec72021d7492222556b6f7547a
aishtiaq <awshaf_ishtiaque@sfu.ca>
2024-07-06 14:46:31 -0700

    update

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const email = document.getElementById("email");
     const password = document.getElementById("password");
+    const togglePasswordVisibility = document.querySelector(".toggle-password-visibility");
   
     //email focus 
     email.addEventListener("focus", (event) => {
@@ -17,5 +18,17 @@ document.addEventListener("DOMContentLoaded", () => {
     password.addEventListener("blur", (event) => {
       event.target.closest('.password-wrapper').classList.remove('focus');
     });
+
+    //eye functionality 
+    togglePasswordVisibility.addEventListener("click", () => {
+        password.focus();
+        if (password.type === "password") {
+          password.type = "text";
+          togglePasswordVisibility.style.backgroundImage = "url('../images/eye.svg')"; // Optional: Change icon to eye-slash when visible
+        } else {
+          password.type = "password";
+          togglePasswordVisibility.style.backgroundImage = "url('../images/eye-closed.svg')"; // Change back to eye icon
+        }
+      });
   });
   
